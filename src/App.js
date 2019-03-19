0935867f568ee3659a81f7f70a519eb7bad9c22b
Emmanuel Olivo <emmanuelo1002@gmail.com>
2019-03-19 14:30:32 -0500

    Add Contact and Header Components with Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The App Component</h1>
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contact
+            name="John Doe"
+            email="johndoe@gmail.com"
+            phone="123-456-7890"
+          />
+          <Contact
+            name="Emm Olivo"
+            email="emm@gmail.com"
+            phone="135-523-3663"
+          />
+        </div>
       </div>
     );
   }
