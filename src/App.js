763990367acd0aa95828ed0055dda8363995415e
Emmanuel Olivo <emmanuelo1002@gmail.com>
2019-03-19 15:57:03 -0500

    Refactor Contacts component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Contact from "./components/Contact";
+import Contacts from "./components/Contacts";
 import Header from "./components/Header";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -11,16 +11,7 @@ class App extends Component {
       <div className="App">
         <Header branding="Contact Manager" />
         <div className="container">
-          <Contact
-            name="John Doe"
-            email="johndoe@gmail.com"
-            phone="123-456-7890"
-          />
-          <Contact
-            name="Emm Olivo"
-            email="emm@gmail.com"
-            phone="135-523-3663"
-          />
+          <Contacts />
         </div>
       </div>
     );
