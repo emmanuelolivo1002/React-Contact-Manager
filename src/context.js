459e076002a009e34cf439b6a29172841982e7ee
Emmanuel Olivo <emmanuelo1002@gmail.com>
2019-03-20 11:11:08 -0500

    Add axios to get placeholder users

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 const Context = React.createContext();
 
@@ -23,30 +24,16 @@ const reducer = (state, action) => {
 
 export class Provider extends Component {
   state = {
-    contacts: [
-      {
-        id: 1,
-        name: "John Doe",
-        email: "jdoe@email.com",
-        phone: "555-555-5555"
-      },
-      {
-        id: 2,
-        name: "Karen Williams",
-        email: "karen@email.com",
-        phone: "333-333-3333"
-      },
-      {
-        id: 3,
-        name: "Henry Johnson",
-        email: "henry@email.com",
-        phone: "222-222-2222"
-      }
-    ],
+    contacts: [],
     dispatch: action => {
       this.setState(state => reducer(state, action));
     }
   };
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(res => this.setState({ contacts: res.data }));
+  }
   render() {
     return (
       <Context.Provider value={this.state}>
