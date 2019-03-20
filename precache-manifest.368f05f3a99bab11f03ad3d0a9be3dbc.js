310904856e7280b61a4446d48a40d65c1c95b3f7
Emmanuel Olivo <emmanuelo1002@gmail.com>
2019-03-20 12:10:25 -0500

    Updates

---
+++ precache-manifest.368f05f3a99bab11f03ad3d0a9be3dbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "45fcd53695b1f471a0f9",
+    "url": "/React-Contact-Manager/static/js/runtime~main.f1d3c017.js"
+  },
+  {
+    "revision": "4942b8d0df2b8c369636",
+    "url": "/React-Contact-Manager/static/js/main.f44ba263.chunk.js"
+  },
+  {
+    "revision": "e20b26a96801e932fae9",
+    "url": "/React-Contact-Manager/static/js/2.cbda7221.chunk.js"
+  },
+  {
+    "revision": "4942b8d0df2b8c369636",
+    "url": "/React-Contact-Manager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "e20b26a96801e932fae9",
+    "url": "/React-Contact-Manager/static/css/2.11220e72.chunk.css"
+  },
+  {
+    "revision": "4b6acace42ab0f0e0c3edd1cf61e93e1",
+    "url": "/React-Contact-Manager/index.html"
+  }
+];
